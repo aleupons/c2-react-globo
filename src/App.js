7fc86e68dd4bf120b584bedb1d4addc4c74f82d0
aleupons <apahoquei@gmail.com>
2021-06-10 12:14:50 +0200

    Funcionalitats del joc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 function App() {
-  return;
+  const [posicioActual, setPosicioActual] = useState({
+    x: Math.floor(Math.random() * window.innerWidth),
+    y: Math.floor(Math.random() * window.innerHeight),
+  });
+  const moureGlobus = () => {
+    setTimeout(() => {
+      setPosicioActual({
+        x: Math.floor(Math.random() * window.innerWidth),
+        y: Math.floor(Math.random() * window.innerHeight),
+      });
+    }, 300);
+  };
+  const [marcador, setMarcador] = useState(0);
+  return (
+    <>
+      <p>Marcador: {marcador} punts</p>
+      <i
+        className="globo fab fa-fly"
+        onMouseOver={moureGlobus}
+        onClick={() => setMarcador(marcador + 1)}
+        style={{ top: posicioActual.y, left: posicioActual.x }}
+      ></i>
+    </>
+  );
 }
 
 export default App;
